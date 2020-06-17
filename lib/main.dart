63d3bd147228a74e5e0dc883dee9ff0e80a75dc0
Nurulloh <nurulloh@mysuperdispatch.com>
2020-06-18 02:23:47 +0500

    additional info card added

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
+          backgroundColor: Colors.blue[800],
           appBar: AppBar(
-            backgroundColor: Colors.white,
+            backgroundColor: Colors.blue[900],
             title: Text(
               'VisitCa',
               style: TextStyle(
-                  letterSpacing: 1.0, color: Colors.black, fontSize: 20),
+                  letterSpacing: 1.0, color: Colors.white, fontSize: 20),
             ),
           ),
           body: Column(
@@ -33,9 +34,16 @@ class MyApp extends StatelessWidget {
                 child: Text(
                   'Jahongir Umarov',
                   style: TextStyle(
-                    fontSize: 40.0,
-                    fontFamily: 'Pacifico'
-                  ),
+                      fontSize: 40.0,
+                      fontFamily: 'Lobster',
+                      color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+                width: 300,
+                child: Divider(
+                  color: Colors.white70,
                 ),
               ),
               Container(
@@ -44,8 +52,67 @@ class MyApp extends StatelessWidget {
                   'BUSINESS OWNER',
                   style: TextStyle(
                       fontSize: 16.0,
-                    fontFamily: 'Comfortaa'
-                  ),
+                      fontFamily: 'Comfortaa',
+                      color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.white54,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 24),
+                color: Colors.black12,
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.white70,
+                      ),
+                      title: Text(
+                        'example@email.com',
+                        style: TextStyle(
+                            color: Colors.white, fontFamily: 'Comfortaa'),
+                      ),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.white70,
+                      ),
+                      title: Text(
+                        '+998990000000',
+                        style: TextStyle(
+                            color: Colors.white, fontFamily: 'Comfortaa'),
+                      ),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.group,
+                        color: Colors.white70,
+                      ),
+                      title: Text(
+                        'linkedin.com/in/username',
+                        style: TextStyle(
+                            color: Colors.white, fontFamily: 'Comfortaa'),
+                      ),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.language,
+                        color: Colors.white70,
+                      ),
+                      title: Text(
+                        'fb.com/username',
+                        style: TextStyle(
+                            color: Colors.white, fontFamily: 'Comfortaa'),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
